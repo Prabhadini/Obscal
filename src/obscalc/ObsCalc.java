5483bec52b31db7551f5eadc610a4644b87edfc3
MERC-LAB-01 <MERC-LAB-01@MERC-LAB-03>
2017-01-19 14:29:40 +0530

    First Commit

---
+++ src/obscalc/ObsCalc.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package obscalc;
+
+/**
+ *
+ * @author MERC-LAB-01
+ */
+public class ObsCalc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      FrameMain mother = new FrameMain();
+      mother.setVisible(true);
+    }
+    
+}
